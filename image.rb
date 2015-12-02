085a0931611b33d4dec3a72e48ad95765c8af67d
jake08bennett <jake08bennett@hotmail.com>
2015-12-02 10:53:28 -0700

    images show randomly

--- image.rb
+++ image.rb
@@ -3,23 +3,23 @@ class Image
 	attr_accessor :x, :y
 	
 	def initialize(x, y)
-		@image = Gosu::Image.new("images/dog.jpg")
+		@image = Gosu::Image.new(random_picture)
 		@x = x
 		@y = y
 		
 	end
 
-	# def random_picture
-	# 	random_selection_array = [1, 2, 3]
-	# 	random_picture = random_selection_array.shuffle.sample
-	# 	if random_picture == 1
-	# 		@img = ("images/tiger.jpg")
-	# 	elsif random_picture == 2
-	# 		@img = ("images/dog.jpg")
-	# 	else
-	# 		@img = ("images/bunnies.jpg")
-	# 	end
-	# end
+	def random_picture
+		random_selection_array = [1, 2, 3]
+	 	random_picture = random_selection_array.shuffle.sample
+	 	if random_picture == 1
+	 		@img = ("images/elephant.jpg")
+	 	elsif random_picture == 2
+	 		@img = ("images/piglet.jpg")
+	 	else
+	 		@img = ("images/polarbear.jpg")
+	 	end
+	end
 
 	def draw
 		@image.draw(@x, @y, 1)
--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ class BanterQuiz < Gosu::Window
  	def initialize
     super 1500, 844
     @background_image = Gosu::Image.new("images/gameshow.jpg")
-   	@round_image = Image.new(750, 422)
+   	@round_image = Image.new(560, 100)
 	end
 
   def update
