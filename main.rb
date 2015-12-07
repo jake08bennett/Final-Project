6a021499f1aa1416cd98c4841e511834b1fae89a
jake08bennett <jake08bennett@hotmail.com>
2015-12-07 10:07:46 -0700

    Renamed image, new branch merged, created scoring branch

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require "gosu"
-require_relative "image"
+require_relative "round"
 require_relative "options"
 
 class BanterQuiz < Gosu::Window
@@ -7,7 +7,7 @@ class BanterQuiz < Gosu::Window
  	def initialize
     super 1500, 844
     @background_image = Gosu::Image.new("images/gameshow.jpg")
-   	@round_image = Image.new(560, 100)
+   	@round_image = Round.new(560, 100)
    	@option_1 = Options.new(585, 500, Gosu::Image.new("images/Option-1.png"))
    	@option_2 = Options.new(585, 620, Gosu::Image.new("images/Option-2.png"))
    	@option_3 = Options.new(800, 500, Gosu::Image.new("images/Option-3.png"))
--- round.rb
+++ round.rb
@@ -1,4 +1,4 @@
-class Image
+class Round
 
 	attr_accessor :x, :y
 	
@@ -6,7 +6,7 @@ class Image
 		@image = Gosu::Image.new(random_picture)
 		@x = x
 		@y = y
-		
+		@score = 0
 	end
 
 	def random_picture
@@ -14,12 +14,26 @@ class Image
 	 	random_picture = random_selection_array.shuffle.sample
 	 	if random_picture == 1
 	 		@img = ("images/elephant.jpg")
+	 		elephant = true
 	 	elsif random_picture == 2
 	 		@img = ("images/piglet.jpg")
+	 		piglet = true
 	 	else
 	 		@img = ("images/polarbear.jpg")
+	 		polarbear = true
 	 	end
 	end
+
+	def button_down
+		if button_down?(KbW) and polarbear == true
+			@score += 25
+		elsif button_down?(KbE) and piglet == true
+			@score += 25
+		elsif button_down?(KbR) and elephant == true
+			@score += 25
+		end
+	end
+
 
 	def draw
 		@image.draw(@x, @y, 1)
